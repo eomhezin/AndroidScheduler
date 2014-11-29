b3acf33453ab31044b1c359c1736c02f03f2ad5c
Junhyung Jo <cho13234@gmail.com>
2014-11-29 16:22:29 +0900

    Intent class data 전송을 위한 Parcelable Interface 구현

--- Android/src/com/example/androidscheduler/ClassInfo.java
+++ Android/src/com/example/androidscheduler/ClassInfo.java
@@ -1,24 +1,76 @@
 package com.example.androidscheduler;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class ClassInfo {
+import android.R.string;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ClassInfo implements Parcelable{
 	
 	public String name;
-	public ArrayList<String> day;		// day - number ex) mon - 2, mon - 3
+	public List<String> day;		// day - number ex) mon - 2, mon - 3
 	public ArrayList<Integer> number;
 	public String detail;
 	
 	public ClassInfo(String name, String day,
 			Integer number, String detail) {
 		
-		this.day = new ArrayList<String>();
-		this.number = new ArrayList<Integer>();
-		
 		this.name = name;
+		if(day == null){
+			this.day = new ArrayList();
+		}
 		this.day.add(day);
+		if(number == null){
+			this.number = new ArrayList();
+		}
 		this.number.add(number);
 		this.detail = detail;
+	}
+	
+	public ClassInfo(Parcel src){
+		
+		name = src.readString();
+		
+		if(day == null){
+			day = new ArrayList();
+		}
+		src.readStringList(day);
+		
+		if(number == null){
+			number = new ArrayList<Integer>();
+		}
+		number = (ArrayList<Integer>)src.readSerializable();
+		
+		detail = src.readString();
+	}
+	
+	public static final Parcelable.Creator CREATOR = new Parcelable.Creator() {
+
+		@Override
+		public Object createFromParcel(Parcel source) {
+			return new ClassInfo(source);
+		}
+
+		@Override
+		public Object[] newArray(int size) {
+			return new ClassInfo[size];
+		}
+		
+	};
+	
+	@Override
+	public int describeContents() {
+		return 0;
+	}
+
+	@Override
+	public void writeToParcel(Parcel dest, int flags) {
+		dest.writeString(name);
+		dest.writeList(day);
+		dest.writeSerializable(number);
+		dest.writeString(detail);
 	}
 
 	public String getName() {
@@ -30,7 +82,7 @@ public class ClassInfo {
 	}
 
 	public ArrayList<String> getDay() {
-		return day;
+		return (ArrayList<String>) day;
 	}
 
 	public void setDay(ArrayList<String> day) {
@@ -52,5 +104,5 @@ public class ClassInfo {
 	public void setDetail(String detail) {
 		this.detail = detail;
 	}
-	
+
 }
